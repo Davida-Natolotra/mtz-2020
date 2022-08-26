160cc9112a2fab5e5d85ec63ed870e9164313098
Dina <dina@MacBook-Pro-de-Dina-Ranaivo.local>
2022-08-26 15:22:40 +0300

    New point control caisse

--- caisseMoto/views.py
+++ caisseMoto/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from asgiref.sync import sync_to_async
 from .models import CaisseMoto, Solde
 from .serializers import CaisseMotoSerializer, SoldeSerializer
+import datetime
+
 
 @sync_to_async
 @api_view(['GET'])
@@ -12,12 +14,14 @@ def list_all_caisse(request):
     serializer = CaisseMotoSerializer(Caisses, many=True)
     return Response(serializer.data)
 
+
 @sync_to_async
 @api_view(['GET'])
 def get_caisse(request, pk=None):
     caisse = CaisseMoto.objects.get(pk=pk)
-    serializer = CaisseMotoSerializer(caisse,many=True)
+    serializer = CaisseMotoSerializer(caisse, many=True)
     return Response(serializer.data)
+
 
 @sync_to_async
 @api_view(['PUT'])
@@ -31,6 +35,7 @@ def update_caisse_moto(request, pk):
         return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 @sync_to_async
 @api_view(['DELETE'])
 def delete_caisse_moto(request, pk=None):
@@ -39,6 +44,7 @@ def delete_caisse_moto(request, pk=None):
     Caisses = CaisseMoto.objects.all()
     serializer = CaisseMotoSerializer(Caisses, many=True)
     return Response(serializer.data)
+
 
 @sync_to_async
 @api_view(['POST'])
@@ -51,27 +57,37 @@ def create_caisse_moto(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 @sync_to_async
 @api_view(['POST'])
 def create_or_update_moto(request):
     id_moto = request.data["id_moto"]
-    try:
-        tofind = CaisseMoto.objects.get(id_moto = id_moto)
-        serializer = CaisseMotoSerializer(instance=tofind, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            Caisses = CaisseMoto.objects.all()
-            serializer = CaisseMotoSerializer(Caisses, many=True)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    except:
-        serializer = CaisseMotoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            Caisses = CaisseMoto.objects.all()
-            serializer = CaisseMotoSerializer(Caisses, many=True)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    date = request.data["date"]
+    print(date)
+    datesplit = date.split("-")
+    dateformat = datetime.datetime(int(datesplit[0]), int(datesplit[1]), int(datesplit[2]))
+    print(f"dateformat: {dateformat}")
+    pointControl = datetime.datetime(2022, 8, 26)
+    if dateformat > pointControl:
+        try:
+            tofind = CaisseMoto.objects.get(id_moto=id_moto)
+            serializer = CaisseMotoSerializer(instance=tofind, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                Caisses = CaisseMoto.objects.all()
+                serializer = CaisseMotoSerializer(Caisses, many=True)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            serializer = CaisseMotoSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                Caisses = CaisseMoto.objects.all()
+                serializer = CaisseMotoSerializer(Caisses, many=True)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response([{}], status=status.HTTP_201_CREATED)
 
 
 @sync_to_async
@@ -81,11 +97,12 @@ def get_solde_all(request):
     serializer = SoldeSerializer(solde, many=True)
     return Response(serializer.data[0])
 
+
 @sync_to_async
 @api_view(['PUT'])
 def update_solde_moto(request):
     solde = Solde.objects.last()
-    serializer = SoldeSerializer(instance=solde,data=request.data)
+    serializer = SoldeSerializer(instance=solde, data=request.data)
     if serializer.is_valid():
         serializer.save()
         solde = Solde.objects.all()
