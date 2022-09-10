b3238ed3a60aa298345be23c8a6f1d0b34188ec2
Dina <dina@MacBook-Pro-de-Dina-Ranaivo.local>
2022-09-10 14:24:07 +0300

    JWT

--- DJ1/settings.py
+++ DJ1/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = '_ikeushbie0s&b-vv8p(4+67a6vt4wf3bwe4)^xr+yq)5f0$7!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','*','192.168.83.129']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*', '192.168.83.129']
 
 
 # Application definition
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
     'compagnie',
     'operation',
     'facture',
-    'django.contrib.sites', # < here
-    'allauth', # < here
-    'allauth.account', # < here
+    'django.contrib.sites',  # < here
+    'allauth',  # < here
+    'allauth.account',  # < here
     'allauth.socialaccount',
     'accounts',
     'commercial',
@@ -51,11 +51,11 @@ INSTALLED_APPS = [
     'corsheaders'
 ]
 
-SITE_ID = 1 # < here
-LOGIN_REDIRECT_URL = '/' # < here
+SITE_ID = 1  # < here
+LOGIN_REDIRECT_URL = '/'  # < here
 
 MIDDLEWARE = [
-    
+
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -164,13 +164,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 INTERNAL_IPS = [
-    
-    '127.0.0.1','localhost'
-   
+
+    '127.0.0.1', 'localhost'
+
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_WHITELIST = [
-    "https://tiamoto.web.app","https://tiamoto.com"
+    "https://tiamoto.web.app", "https://tiamoto.com"
 ]
 
 REST_FRAMEWORK = {
@@ -181,7 +181,32 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=30), 
-    'ROTATE_REFRESH_TOKENS': True,
-    'UPDATE_LAST_LOGIN': True,
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
